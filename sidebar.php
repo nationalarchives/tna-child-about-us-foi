ba355abcbdfa3987e8a504cccc9d2ddb56d59149
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-04-04 14:10:47 +0100

    Fixed the sidebar title and it's permalink

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,24 @@
 <?php
 $defaults = array(
-    'theme_location'  => 'sidebar-menu',
-    'menu'            => 'Sidebar Menu',
-    'items_wrap'      => '<ul class="sibling">%3$s</ul>', );
+    'theme_location' => 'sidebar-menu',
+    'menu' => 'Sidebar Menu',
+    'items_wrap' => '<ul class="sibling">%3$s</ul>',);
+
+// Global variable
+global $pre_crumbs;
+// Slicing the array inside $pre_crumbs variable and displaying the key name
+$sidebar_title = array_slice($pre_crumbs, -1);
 ?>
 
 <aside id="sidebar" class="col-xs-12 col-sm-4 col-md-4" role="complementary">
     <div class="sidebar-header">
         <h2>
-            <a name="inThisSection" href="<?php echo make_path_relative( get_permalink($parent_page_id) ); ?>">
-                Also in <?php echo get_the_title($parent_page_id);?>
+            <a name="inThisSection" href="<?php echo next($pre_crumbs) ?>">
+                Also in <?php echo key($sidebar_title); ?>
             </a>
         </h2>
     </div>
     <div class="sidebar-nav clearfix">
-        <?php wp_nav_menu( $defaults ); ?>
+        <?php wp_nav_menu($defaults); ?>
     </div>
 </aside>
